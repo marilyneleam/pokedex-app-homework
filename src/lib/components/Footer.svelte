0233f73ad03673c085b2d555ce73c3375025e04f
marilyne.leam <marilyne.leam@eemi.com>
2024-07-07 16:38:32 +0200

    add header and footer without styles

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,4 @@
+<footer>
+  <p>&copy; 2024 App Pokémon by Marilyne LEAM. Tous droits réservés.</p>
+  <p>Données fournies par <a href="https://pokeapi.co/" target="_blank" rel="noopener noreferrer">PokeAPI</a></p>
+</footer>
---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,12 @@
+<header>
+	<nav>
+		<ul>
+			<li>
+				<a href="/">Accueil</a>
+			</li>
+			      <li>
+        <a href="/search">Recherche</a>
+      </li>
+		</ul>
+	</nav>
+</header>

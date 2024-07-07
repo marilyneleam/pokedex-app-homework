75bb070c3b2eaa6c2eb8634885a8c99793657241
marilyne.leam <marilyne.leam@eemi.com>
2024-07-07 23:14:39 +0200

    add route search

---
+++ src/routes/search/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import SearchBar from '$lib/components/SearchBar.svelte'
+  import '../../app.css'
+</script>
+
+<div class="container mx-auto px-4">
+  <h1 class="text-3xl font-bold text-center my-8">Recherche Pokémon</h1>
+  <div>
+    <SearchBar />
+  </div>
+</div>

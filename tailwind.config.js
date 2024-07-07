e15d65a533e4d2a34ab2469c4c7b7364558b9de1
marilyne.leam <marilyne.leam@eemi.com>
2024-07-07 23:20:26 +0200

    file config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        pokemon: {
+          normal: '#A8A878',
+          fire: '#F08030',
+          water: '#6890F0',
+          electric: '#F8D030',
+          grass: '#78C850',
+          ice: '#98D8D8',
+          fighting: '#C03028',
+          poison: '#A040A0',
+          ground: '#E0C068',
+          flying: '#A890F0',
+          psychic: '#F85888',
+          bug: '#A8B820',
+          rock: '#B8A038',
+          ghost: '#705898',
+          dragon: '#7038F8',
+          dark: '#705848',
+          steel: '#B8B8D0',
+          fairy: '#EE99AC',
+        },
+      },
+    },
+  },
+  plugins: [
+    function({ addBase, theme }) {
+      addBase({
+        ':root': Object.entries(theme('colors.pokemon')).reduce((vars, [name, color]) => {
+          vars[`--pokemon-${name}`] = color;
+          return vars;
+        }, {})
+      });
+    },
+  ],
+}
